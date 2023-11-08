cc18a253fafd543cfd5e8173d8452c8a78c74fbe
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-11-08 09:19:20 +0200

    enrolled

--- mindx-dashboard/src/Components/EnrolledStudents.jsx
+++ mindx-dashboard/src/Components/EnrolledStudents.jsx
@@ -1,41 +1,141 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 const EnrolledStudents = () => {
+  const [teacherId, setTeacherId] = useState("");
+
+  useEffect(() => {
+    // Retrieve the teacherId from the cookie when the component mounts
+    const storedTeacherId = Cookies.get("teacherId");
+
+    if (storedTeacherId) {
+      setTeacherId(storedTeacherId);
+    }
+  }, []);
+  const url = "http://localhost:5000";
+  const [data, setData] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const fetchData = () => {
+    const teacherId = Cookies.get("teacherId");
+    const apiUrl = url + `/language/getLanguages/${teacherId}`;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setData(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const deleteStudent = (id) => {
+    axios
+      .delete(url + `/studentInfo/deleteStudentInfo/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        fetchData();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handleDeleteClick = (id) => {
+    setSelectedId(id);
+    setIsModalOpen(true);
+  };
+
+  const handleDeleteConfirm = () => {
+    deleteStudent(selectedId);
+    setIsModalOpen(false);
+    setSelectedId(null);
+  };
+
+  const handleDeleteCancel = () => {
+    setIsModalOpen(false);
+    setSelectedId(null);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-  
-      <div>
-      <p className='content-title'>Student's Enrolled In Your Courses</p>
-      <table className='content-table'>
+    <div>
+      <p className="content-title">Appoitment</p>
+      <table className="content-table">
         <thead>
           <tr>
-            <th className='content-data'>Student Id</th>
-            <th className='content-data'>Name</th>
-            <th className='content-data'>Email</th>
-            <th className='content-data'>Password</th>
-            <th className='content-data'>Action</th>
+            <th className="content-data">Student Id</th>
+            <th className="content-data">Name</th>
+            <th className="content-data"></th>
+            <th className="content-data">Appoitment_End_Time</th>
+            <th className="content-data">Status </th>
+            <th className="content-data">Student_ID</th>
+            <th className="content-data">Teacher_ID</th>
           </tr>
         </thead>
         <tbody>
-         
-              <tr key="">
-                <td className='content-data'></td>
-                <td className='content-data'></td>
-                <td className='content-data'></td>
-                <td className='content-data'></td>
-                <td className='content-data'>
+          {data &&
+            data.map((data1) => (
+              <tr key={data1.appoitment_id}>
+                <td className="content-data">{data1.appoitment_name}</td>
+                <td className="content-data">{data1.appoitment_date}</td>
+                <td className="content-data">{data1.appoitment_start_time}</td>
+                <td className="content-data">{data1.appoitment_end_time}</td>
+                <td className="content-data">{data1.status}</td>
+                <td className="content-data">{data1.student_id}</td>
+                <td className="content-data">{data1.teacher_id}</td>
+                <td className="content-data">
                   <button
-                    className='action-button delete'
-                    onClick={() => ""}
+                    className="action-button delete"
+                    onClick={() => handleDeleteClick(data1.appoitment_id)}
                   >
                     Delete
                   </button>
                 </td>
               </tr>
-        
+            ))}
         </tbody>
       </table>
-  </div>
-  )
-}
 
-export default EnrolledStudents
+      {isModalOpen && (
+        <div
+          className="modal"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <div className="modal-background"></div>
+          <div className="modal-content">
+            <div className="box">
+              <p>Are you sure you want to delete this item?</p>
+              <button
+                className="button is-danger"
+                onClick={handleDeleteConfirm}
+              >
+                Delete
+              </button>
+              <button className="button" onClick={handleDeleteCancel}>
+                Cancel
+              </button>
+            </div>
+          </div>
+          <button
+            className="modal-close is-large"
+            aria-label="close"
+            onClick={handleDeleteCancel}
+          ></button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EnrolledStudents;
