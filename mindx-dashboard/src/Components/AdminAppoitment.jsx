ad04a0f85e5123d4a3604eebfbecb0f459172df0
Fatima Tfaily <fatima.tf2005@gmail.com>
2023-11-09 09:34:06 +0200

    EDIT

--- mindx-dashboard/src/Components/AdminAppoitment.jsx
+++ mindx-dashboard/src/Components/AdminAppoitment.jsx
@@ -55,18 +55,8 @@ const Appoitment = () => {
     <div>
       <p className="content-title">Appoitment</p>
       <table className="content-table">
-    <div>
-      <p className="content-title">Appoitment</p>
-      <table className="content-table">
         <thead>
           <tr>
-            <th className="content-data">Appoitment_Name</th>
-            <th className="content-data">Appoitment_Date</th>
-            <th className="content-data">Appoitment_Start_Time</th>
-            <th className="content-data">Appoitment_End_Time</th>
-            <th className="content-data">Status </th>
-            <th className="content-data">Student_ID</th>
-            <th className="content-data">Teacher_ID</th>
             <th className="content-data">Appoitment_Name</th>
             <th className="content-data">Appoitment_Date</th>
             <th className="content-data">Appoitment_Start_Time</th>
@@ -95,26 +85,7 @@ const Appoitment = () => {
                     Delete
                   </button>
                 </td>
-          {data &&
-            data.map((data1) => (
-              <tr key={data1.appoitment_id}>
-                <td className="content-data">{data1.appoitment_name}</td>
-                <td className="content-data">{data1.appoitment_date}</td>
-                <td className="content-data">{data1.appoitment_start_time}</td>
-                <td className="content-data">{data1.appoitment_end_time}</td>
-                <td className="content-data">{data1.status}</td>
-                <td className="content-data">{data1.student_id}</td>
-                <td className="content-data">{data1.teacher_id}</td>
-                <td className="content-data">
-                  <button
-                    className="action-button delete"
-                    onClick={() => handleDeleteClick(data1.appoitment_id)}
-                  >
-                    Delete
-                  </button>
-                </td>
               </tr>
-            ))}
             ))}
         </tbody>
       </table>
@@ -155,41 +126,4 @@ const Appoitment = () => {
   );
 };
 
-      {isModalOpen && (
-        <div
-          className="modal"
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100vh",
-          }}
-        >
-          <div className="modal-background"></div>
-          <div className="modal-content">
-            <div className="box">
-              <p>Are you sure you want to delete this item?</p>
-              <button
-                className="button is-danger"
-                onClick={handleDeleteConfirm}
-              >
-                Delete
-              </button>
-              <button className="button" onClick={handleDeleteCancel}>
-                Cancel
-              </button>
-            </div>
-          </div>
-          <button
-            className="modal-close is-large"
-            aria-label="close"
-            onClick={handleDeleteCancel}
-          ></button>
-        </div>
-      )}
-    </div>
-  );
-};
-
-export default Appoitment;
 export default Appoitment;
